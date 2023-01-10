7b43e1f4708b66b976444f67c5c802fecc3ca558
camilo londoño <cl.mon9566@gmail.com>
2023-01-10 16:13:20 -0500

    gif-finder

---
+++ GifExpertApp/src/components/AddCategory.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+export const AddCategory = ({ onNewCategory }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const onImputChange = ({ target }) => {
+    setInputValue(target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (inputValue.trim().length <= 1) return;
+    setInputValue('');
+    onNewCategory(inputValue.trim());
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type='text'
+        placeholder='Search Gifs'
+        value={inputValue}
+        onChange={onImputChange}
+      />
+    </form>
+  );
+};
